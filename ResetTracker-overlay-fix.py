d30a53435f3b74ce28be2b744cb6033510dba129
baf <yuanqiwei71@gmail.com>
2023-12-29 17:01:52 +0900

    デバッグ用のprintをコメントアウト

--- ResetTracker-overlay-fix.py
+++ ResetTracker-overlay-fix.py
@@ -7,24 +7,24 @@ fix_paths = ['obs/EnterAvg_fix.txt', 'obs/NPH_fix.txt']
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        print(f"File {event.src_path} has been modified")
+        # print(f"File {event.src_path} has been modified")
         if os.path.basename(event.src_path) == os.path.basename(file_paths[0]):
-            print("enterAvg")
+            # print("enterAvg")
             with open(event.src_path, mode='r') as f1:
                 self.data = float(f1.read())
-                print(round(self.data, 1))
+                # print(round(self.data, 1))
                 self.fixed_enterAvg = self.time_format(self.data)
-                print(self.fixed_enterAvg)
+                # print(self.fixed_enterAvg)
                 with open(fix_paths[0], 'w+') as f2:
                     f2.write(self.fixed_enterAvg)
 
         elif os.path.basename(event.src_path) == os.path.basename(file_paths[1]):
-            print("NPH")
+            # print("NPH")
             with open(event.src_path, mode='r') as f1:
                 self.data = float(f1.read())
-                print(self.data)
+                # print(self.data)
                 self.fixed_nph = round(self.data, 2)
-                print(self.fixed_nph)
+                # print(self.fixed_nph)
                 with open(fix_paths[0], 'w+') as f2:
                     f2.write(str(self.fixed_nph))
                 
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     event_handler = MyHandler()
     observer = Observer()
 
-    print("start")
+    print("starting")
 
     observer.schedule(event_handler, path='obs', recursive=False)
 
@@ -63,3 +63,4 @@ if __name__ == "__main__":
         observer.stop()
     finally:
         observer.stop()
+        print("stopping")
